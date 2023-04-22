3722e7dca840c7b59423d017b6b0dd4db178cf20
Eric Furrevig <efurrevig@gmail.com>
2023-04-22 16:50:03 -0400

    Get/post requests modified for MongoDB

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
+
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
-const Note = require('./models/note')
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -31,34 +26,28 @@ app.get('/api/notes', (request, response) => {
 app.post('/api/notes/', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 
 //get individual note with id #
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 //change note
@@ -81,7 +70,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
--- models/note.js
+++ models/note.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = proccess.env.MONGODB_URI
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
