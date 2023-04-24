d243e8eaf2c4750aa6ca0e75425f10117ef06f9b
Eric Furrevig <efurrevig@gmail.com>
2023-04-23 20:01:19 -0400

    Added eslint

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/api/notes', (request, response) => {
 
 
 //create note
-app.post('/api/notes/', (request, response) => {
+app.post('/api/notes/', (request, response, next) => {
     const body = request.body
 
     if (body.content === undefined) {
@@ -39,34 +39,34 @@ app.post('/api/notes/', (request, response) => {
         important: body.important || false,
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 
 //get individual note with id #
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
         if (note) {
             response.json(note)
         } else {
             response.status(404).end()
         }
-    })
-    .catch(error => next(error))
+    }).catch(error => next(error))
 })
 
 //change note
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const { content, important } = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, {new: true })
+    Note.findByIdAndUpdate(
+        request.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: 'query' }
+    )
         .then(updatedNote => {
             response.json(updatedNote)
         })
@@ -90,14 +90,16 @@ app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
-  
+
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
     next(error)
-  }
-  
+}
+
 app.use(errorHandler)
 
 
